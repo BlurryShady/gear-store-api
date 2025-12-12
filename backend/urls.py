99f9891abfd1f976834c44227cf4e8b76c21bd5a
BlurryShady <blurryshady@hotmail.com>
2025-12-12 22:08:40 +0300

    Integrated media

--- backend/urls.py
+++ backend/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import JsonResponse
+import os
 
 def health(request):
     return JsonResponse({"status": "ok"})
@@ -12,5 +13,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("store.urls")),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG or os.environ.get("SERVE_MEDIA") == "1":
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
